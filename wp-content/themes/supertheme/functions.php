e3f4990fd55d33213a00e62fa3192868fa0d9bfa
jowel007 <abuhanifjowel1@gmail.com>
2025-01-06 14:12:24 +0600

    diffrent class use in wordpress

---
+++ wp-content/themes/supertheme/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+    add_theme_support( 'menus' );
+    add_theme_support( 'widgets' );
+    add_theme_support('custom-background');
+    add_theme_support( 'custom-header' );
+    add_theme_support('post-thumbnails');
+
+?>
---
+++ wp-content/themes/supertheme/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ADD CSS HERE -->
+
+    <title> <?php bloginfo('title'); ?>
+    </title>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+
+
+
+    <!-- ADD JAVASCRIPT HERE-->
+    <?php wp_footer(); ?>
+</body>
+
+</html>
