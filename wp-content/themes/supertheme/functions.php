a9f226f69ca76e4e7a0d1ea0f19d64186ba83d83
jowel007 <abuhanifjowel1@gmail.com>
2025-01-07 21:58:55 +0600

    dynamic data show and multi menu and post show in dynamic

--- wp-content/themes/supertheme/functions.php
+++ wp-content/themes/supertheme/functions.php
@@ -1,9 +1,15 @@
 <?php
 
-    add_theme_support( 'menus' );
-    add_theme_support( 'widgets' );
-    add_theme_support('custom-background');
-    add_theme_support( 'custom-header' );
-    add_theme_support('post-thumbnails');
+add_theme_support('menus');
+add_theme_support('widgets');
+add_theme_support('custom-background');
+add_theme_support('custom-header');
+add_theme_support('post-thumbnails');
 
-?>
+// register_nav_menu('header menu', 'Header menu'); single menu register
+
+register_nav_menus(array(
+    'header menu' => __('Header Menu'),
+    'footer-menu' => __('Footer Menu'),
+    'sidebar-menu' => __('Sidebar Menu'),
+));
--- wp-content/themes/supertheme/index.php
+++ wp-content/themes/supertheme/index.php
@@ -15,8 +15,36 @@
 
 <body <?php body_class(); ?>>
 
+    <!-- ADD HTML HERE -->
+    <div>
+        <div class='header'>
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'header menu',
+            ));
+            ?>
+        </div>
+
+        <hr><br><br>
+        <div class="post">
+            <?php while (have_posts()) : the_post(); ?>
+                <h2><?php the_title(); ?></h2>
+                <h2><?php the_post_thumbnail(); ?></h2>
+                <p><?php the_content(); ?></p>
+            <?php endwhile; ?>
+        </div>
 
 
+
+        <div class="footer">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'footer-menu',
+            ));
+            ?>
+        </div>
+
+    </div>
 
     <!-- ADD JAVASCRIPT HERE-->
     <?php wp_footer(); ?>
